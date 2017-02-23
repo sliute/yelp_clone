088bc343cf18fabcb2cc8dbf854256b928efbe60
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-23 16:58:49 +0000

    Version 3 complete baby!

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,12 +1,15 @@
 class Restaurant < ApplicationRecord
   belongs_to :user
-  has_many :reviews, dependent: :destroy
+  has_many :reviews,
+      -> { extending WithUserAssociationExtension },
+      dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
-  def build_review(review_params, current_user)
-    review = reviews.new(review_params)
-    review.user = current_user
-    review
+  has_many :reviews do
+    def build_with_user(attributes = {}, user)
+      attributes[:user] ||= user
+      build(attributes)
+    end
   end
 
 end
