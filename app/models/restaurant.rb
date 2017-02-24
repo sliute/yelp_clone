84127d190cfd1ff49a797c47e22e15609cdd0c36
Dylan Rhodius <dylanrhodius@hotmail.com>
2017-02-24 15:07:42 +0000

    Spiked adding images.

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -5,6 +5,9 @@ class Restaurant < ApplicationRecord
       dependent: :destroy
   validates :name, length: { minimum: 3 }, uniqueness: true
 
+  has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+
   has_many :reviews do
     def build_with_user(attributes = {}, user)
       attributes[:user] ||= user
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,11 +2,12 @@ class User < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :restaurants, dependent: :destroy
   has_many :reviewed_restaurants, through: :reviews, source: :restaurant
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, :omniauth_providers => [:facebook]
+         :omniauthable, :omniauth_providers => [:facebook]      
 
   def self.from_omniauth(auth)
   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
